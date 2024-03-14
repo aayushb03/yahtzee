d6f775378b3462204d40a4b486549eef23cdc295
Carmen Polzin <cpolzin@wisc.edu>
2024-03-13 20:36:12 -0500

    documentation added to api files

--- src/app/api/getAllScores/route.ts
+++ src/app/api/getAllScores/route.ts
@@ -1,6 +1,11 @@
 import {NextResponse} from "next/server";
 import prisma from "@/../prisma/client";
 
+// uses prisma client to return all entries in the database called Past_Scores - used at end of game for leaderboard
+// Past_Scores is formatted as such:
+//  | Game_Number (auto_increment) | Player_Name (VARCHAR(45) | Score (INT) |
+// if unsucessfull in fetching, throws error
+
 export async function GET() {
   try {
     const scores = await prisma.past_Scores.findMany();
