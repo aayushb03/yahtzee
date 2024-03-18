c1b7c67b7cc257e0424ae11c8524d6302dd46065
Jess Xu <jxxu2@wisc.edu>
2024-03-18 15:57:37 -0500

    changed board back to init, also added components testing

--- __tests__/frontend/components.test.tsx
+++ __tests__/frontend/components.test.tsx
@@ -2,11 +2,40 @@ import '@testing-library/jest-dom';
 
 import React from 'react';
 import { render, fireEvent, act, waitFor } from '@testing-library/react';
-import Home from '../../src/app/page'
+import Board from '../../src/app/components/board';
+import { LocalPlayers } from '../../__mocks__/localPlayers';
 
 // Mocking services
-jest.mock('../src/services/scoreService', () => ({
+jest.mock('../../src/services/scoreService', () => ({
   getAllScores: jest.fn().mockResolvedValue([]),
   addScore: jest.fn().mockResolvedValue(undefined),
   clearScores: jest.fn().mockResolvedValue(undefined),
 }));
+
+describe('board component', () => {
+  const onScoreSelectMock = jest.fn();
+  // const currPlayersMock = LocalPlayers.mock.instances.map(instance => instance.players);
+
+  console.log(LocalPlayers)
+  test('renders upper section of table correctly', () => {
+    const { getByText } = render(
+    <Board 
+      currentPlayers={LocalPlayers}
+      onScoreSelect={onScoreSelectMock}
+      potentialScores={[]}
+      diceRolled={false}
+      />);
+    expect(getByText('UPPER SECTION')).toBeInTheDocument();
+  });
+
+  test('renders lower section of table correctly', () => {
+      const { getByText } = render(
+      <Board 
+        currentPlayers={LocalPlayers}
+        onScoreSelect={onScoreSelectMock}
+        potentialScores={[]}
+        diceRolled={false}
+        />);
+    expect(getByText('LOWER SECTION')).toBeInTheDocument();
+  });
+});
