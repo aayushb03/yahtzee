61df392eee3bbcb547c892d842c188852d78e6bb
Carmen Polzin <cpolzin@wisc.edu>
2024-03-18 21:05:33 -0500

    highlighted fixed for realzies)

--- src/app/endPageCard.tsx
+++ src/app/endPageCard.tsx
@@ -18,7 +18,7 @@ const EndPageCard = ({players, onRestart} : EndPageCardProps) => {
   const [currentPlayers, setCurrentPlayers] = useState<Player[]>([]);
   // declares leaderboard scores as a list of player object which each have a Game_Num. Player_Name, and Score property
   const [leaderboardScores, setLeaderboardScores] = useState<Score[]>([]);
-  const [recentGameNum, setRecentGameNum] = useState(0);
+  const [recentGameNums, setRecentGameNums] = useState([0]);
 
 
 
@@ -34,8 +34,12 @@ const EndPageCard = ({players, onRestart} : EndPageCardProps) => {
     //want to get the most recent game number (the greatest game number is going to be the largest)
     getAllScores().then((scores: Score[]) => { 
       let largestNums = scores.sort((a, b) => b.Game_Num - a.Game_Num);
-      let firstLargestNum = largestNums.slice(0,1)
-      setRecentGameNum(firstLargestNum[0].Game_Num);
+      let firstLargestNums = largestNums.slice(0,curPlayers.length)
+      let gameNumList=[] 
+      for(let i in firstLargestNums){
+        gameNumList.push(firstLargestNums[i].Game_Num)
+      }
+      setRecentGameNums(gameNumList)
     });
   }, [players]);
 
@@ -79,7 +83,7 @@ const EndPageCard = ({players, onRestart} : EndPageCardProps) => {
                 <div
                   key={index}
                   className={`text-xl flex w-[80%] border-b ${
-                    currentPlayers.some((player) => ((player.name === entry.Player_Name) && (recentGameNum == entry.Game_Num))) ? 'font-bold' : ''
+                    (currentPlayers.some((player) => ((player.name === entry.Player_Name))) && recentGameNums.some((game) => ((game === entry.Game_Num))) )? 'font-bold' : ''
                   }`}
                 >
                   <div className={"text-left w-[50%]"}>
