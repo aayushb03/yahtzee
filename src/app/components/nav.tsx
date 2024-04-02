10b985872a65cf4327dd35bfb17fe219a0635eb6
BENJAMIN PAUL LYNCH <bplynch3@wisc.edu>
2024-04-02 13:40:26 -0500

    minor changes to help button

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -38,8 +38,19 @@ const Nav = () => {
 
       {/* This Modal should appear when isHelpModalOpen is true */}
       <Modal isOpen={isHelpModalOpen} onClose={() => setHelpModalOpen(false)}>
-        <p>Modal content goes here. This should be visible when the modal is open.</p>
-        {/* xxxxxx texts goes here  */}
+        <p><u><b>Basic rule of Yahtzee:</b></u></p>
+        <p>Objective: Score points by rolling 5 dice to make certain combinations</p>
+        <p>Turns: Players takes turns rolling the dice 3 times each turn</p>
+        <p>Scoring: Choose from the following categories to score points</p>
+        <ul>
+          <li>Ones, Twos, Threes, Fours, Fives, Sixes</li>
+          <li>Three of a kind, Four of a kind</li>
+          <li>Full House</li>
+          <li>Small Straight, Large Straight</li>
+          <li>Yahtzee</li>
+          <li>Chance</li>
+        </ul>
+        <p>Winner: the winner is the person with the most points after everyones board is filled up</p>
       </Modal>
 
       {/* This Modal should appear when statsOpen is true */}
