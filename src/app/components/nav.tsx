df28e8380a0be9d2140b2e3015f68355834caedf
Carmen Polzin <cpolzin@wisc.edu>
2024-04-02 13:22:42 -0500

    adding to nav

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -4,6 +4,7 @@ import Modal from "./modal";
 
 const Nav = () => {
   const [isHelpModalOpen, setHelpModalOpen] = useState(false);
+  const [statsOpen, setStatsOpen] = useState(false);
   const iconClasses = "transition transform hover:-translate-y-1 cursor-pointer";
 
   return (
@@ -12,18 +13,33 @@ const Nav = () => {
         <div className="w-[22%]"/>
         <h1 className="text-6xl text-white my-1 w-[50%] text-center [text-shadow:_0_4px_0_rgb(0_0_0_/_40%)]">YAHTZEE</h1>
         <div className="w-[22%] flex justify-end text-3xl text-white gap-4">
+
           {/* When this button is clicked, isHelpModalOpen becomes true, which should show the Modal */}
           <button onClick={() => setHelpModalOpen(true)} className={iconClasses}>
             <IoIosHelpCircleOutline />
           </button>
-          <IoIosSettings className={iconClasses}/>
-          <IoIosStats className={iconClasses}/>
-          <IoIosLogIn className={iconClasses}/>
+
+          {/*<IoIosSettings className={iconClasses}/>*/}
+          
+          <button onClick={() => setHelpModalOpen(true)} className={iconClasses}>
+            <IoIosStats className={iconClasses}/>
+          </button>
+
+          <button onClick={() => setHelpModalOpen(true)} className={iconClasses}>
+            <IoIosLogIn className={iconClasses}/>
+          </button>
+      
         </div>
       </nav>
 
       {/* This Modal should appear when isHelpModalOpen is true */}
       <Modal isOpen={isHelpModalOpen} onClose={() => setHelpModalOpen(false)}>
+        <p>Modal content goes here. This should be visible when the modal is open.</p>
+        {/* xxxxxx texts goes here  */}
+      </Modal>
+
+      {/* This Modal should appear when isHelpModalOpen is true */}
+      <Modal isOpen={statsOpen} onClose={() => setStatsOpen(false)}>
         <p>Modal content goes here. This should be visible when the modal is open.</p>
         {/* xxxxxx texts goes here  */}
       </Modal>
