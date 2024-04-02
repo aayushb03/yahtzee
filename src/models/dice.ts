ef1bb36dfc0abd5664fd6b233403906234c0b4a3
aayushb03 <abbharadwaj@wisc.edu>
2024-04-02 16:17:56 -0500

    Fixed yahtzee bonus for AI

--- src/models/dice.ts
+++ src/models/dice.ts
@@ -53,7 +53,7 @@ export class Dice implements IDice {
     for (let i = 0; i < 5; i++) {
       if (indices[i] == 0) {
         this.dice[i] = Math.floor(Math.random() * 6) + 1;
-        this.dice[i] = 5;
+        // this.dice[i] = 5;
       }
     }
   }
