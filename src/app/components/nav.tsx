2a4789f04a7046356d47bc48dfd21a8c2472e146
Carmen Polzin <cpolzin@wisc.edu>
2024-04-03 16:26:30 -0500

    added navigation back to gameModeCard for logout and logo press + added font change in rules

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -5,17 +5,19 @@ import {Player} from "@/models/player";
 import { getAllScores} from "@/services/scoreService";
 import { Score } from "@/services/scoreService"; 
 import { Baloo_2 } from "next/font/google";
-import {GameStatus as GS} from "@/models/enums";
+import {GameStatus as GS, GameStatus} from "@/models/enums";
 import changePlayersAndReset from '@/app/yahtzeeGame'
 
   // font that we use for titles (not cell text)
 const baloo2 = Baloo_2({ subsets: ["latin"] });
 
+type NavProps = {
+  setGameStatus: (status: GameStatus) => void;
+}
 
-const Nav = () => {
+const Nav = ({setGameStatus} : NavProps) => {
   const iconClasses = "transition transform hover:-translate-y-1 cursor-pointer";
 
-  const [gameStatus, setGameStatus] = useState<GS>(GS.AddPlayers);
   const [players, setPlayers] = useState<Player[]>([]);
 
   // helpers to see if modals are open or not
@@ -43,6 +45,8 @@ const Nav = () => {
    * When logout button or logo is clicked, navigates to homescreen
    */
   const navToHomeScreen = () =>{
+    setPlayers([]);
+    setGameStatus(GS.AddPlayers);
 
   }
 
@@ -69,7 +73,7 @@ const Nav = () => {
           </button>
 
            {/* When this button is clicked, page navigates back to homescreen */}
-          <button onClick={() => navToHomeScreen} className={iconClasses}>
+          <button onClick={() => navToHomeScreen()} className={iconClasses}>
             <IoIosLogIn className={iconClasses}/>
           </button>
       
@@ -79,6 +83,7 @@ const Nav = () => {
       {/* This Modal should appear when isHelpModalOpen is true */}
       <Modal isOpen={isHelpModalOpen} onClose={() => setHelpModalOpen(false)}>
         <p className="text-3xl"><u>Basic rules of Yahtzee:</u></p>
+        <div className={baloo2.className}>
         <p><span style={{ fontSize: '1.5em' }}>Objective: </span> Score points by rolling 5 dice to make certain combinations</p>
         <p><span style={{ fontSize: '1.5em' }}># of Players: </span> Yahtzee requires 2-4 players</p>
         <p><span style={{ fontSize: '1.5em' }}>Turns: </span>Players takes turns rolling the dice 3 times each turn</p>
@@ -94,6 +99,7 @@ const Nav = () => {
         <p><span style={{ fontSize: '1.5em' }}>Yahtzee Bonus!: </span> Each yahtzee after your first gives you a 100 point bonus that takes a unused spot on your scorecard</p>
         <p><span style={{ fontSize: '1.5em' }}>Winner: </span> After 13 rounds, whoever has the most points is the winner!</p>
         <hr style={{ border: '0', borderTop: '2px solid black', width: '50%', margin: '20px auto' }} />
+        </div>
         <div style={{ textAlign: 'center' }}>
           <p style={{ fontSize: '20px' }}>For more detailed rules, please click the link below: </p>
           <a href="https://www.hasbro.com/common/instruct/yahtzee.pdf" target="_blank" rel="noopener noreferrer" style={{ fontSize: '24px', display: 'block', margin: '10px auto', color: 'blue', textDecoration: 'underline' }}>Yahtzee Instructions PDF</a>
