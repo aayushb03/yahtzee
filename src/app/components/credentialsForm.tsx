f3aa259c7c8741b148542a1d52322393c70a8052
Carmen Polzin <cpolzin@wisc.edu>
2024-04-11 12:16:16 -0500

    sign in page up, not running yet

--- src/app/components/credentialsForm.tsx
+++ src/app/components/credentialsForm.tsx
@@ -11,30 +11,57 @@ type CredentialsFormProps = {
 const CredentialsForm = ({csrfToken}: CredentialsFormProps) => {
     const [error, setError] = useState("")
 
-    const handleSubmit = async (e: Event) => {
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const target = e.currentTarget as HTMLFormElement;
         const data = new FormData(target);
     
-        const signInResponse = await signIn("credentials",{
-            username: data.get("text"),
-            password: data.get("password"),
-            redirect: false,
-        })
-
-        if(signInResponse && !signInResponse.error){
-            //do something here
-        }else{
-            console.log("Error: ", signInResponse)
-            setError("Username or password is wrong")
+        const signInResponse = await signIn("credentials", {
+          username: data.get("username") as string,
+          password: data.get("password") as string,
+          redirect: false,
+        });
+    
+        if (signInResponse && !signInResponse.error) {
+          // do something here
+        } else {
+          console.log("Error: ", signInResponse);
+          setError("Username or password is wrong");
         }
-
-    };
+    }
 
     return( 
-        <form
-        onSubmit={handlesubmit}>
+        <div className="flex h-full w-full justify-center items-center mb-40">
+                <div className="flex justify-around">
+                    <form onSubmit={handleSubmit} className="flex flex-col">
+                        <div className="mb-4">
+                            <label htmlFor="username" className="mr-2 text-lg">Username: </label>
+                            <input
+                            type="text"
+                            id="username"
+                            name="username"
+                            className="border border-gray-800 px-2 py-1 text-lg"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label htmlFor="password" className="mr-2 text-lg">Password: </label>
+                            <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            className="border border-gray-800 px-2 py-1 text-lg"
+                            />
+                        </div>
+                        <div>
+                            {error && <div style={{ color: "red", fontSize: "1.5rem" }}>{error}</div>}
+                            <button type="submit" className="bg-blue-500 text-white px-4 py-2 mx-auto">
+                            Sign In
+                            </button>
+                        </div>
+                    </form>
+                </div>
+        </div>
 
-        </form>
-    )
+  );
+
 }; export default CredentialsForm;
