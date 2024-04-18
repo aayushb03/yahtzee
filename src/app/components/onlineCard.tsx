e88a8da761c0292ac7daad5add082e64de83eaca
MENG FONG LIO <mengfong@wisc.edu>
2024-04-17 22:17:08 -0500

    feat: adding fix name on online mode, change the layout and design

--- src/app/components/onlineCard.tsx
+++ src/app/components/onlineCard.tsx
@@ -3,6 +3,7 @@
 import {useEffect, useState} from "react";
 import {IOnlinePlayer, createGameRoom, getAllPlayers, joinGameRoom} from "@/services/onlineGameService";
 import OnlinePlayerList from "@/app/components/onlinePlayerList";
+import { useUser } from '@/services/userContext';
 
 export const OnlineCard = () => {
   const [gameIdInput, setGameIdInput] = useState<string>("");
@@ -13,7 +14,20 @@ export const OnlineCard = () => {
   const [error, setError] = useState<string>("");
   const [isHost, setIsHost] = useState<boolean>(false);
   const [isReady, setIsReady] = useState<boolean>(false);
+  const { user } = useUser(); 
 
+  /**
+   * Sets the player name to the user's username if it exists.
+   */
+  useEffect(() => {
+    if (user && user.username) {
+      setPlayerName(user.username);
+    }
+  }, [user]);
+
+  /**
+   * Gets all players in the game room when the game is joined.
+   */
   useEffect(() => {
     if (gameJoined) {
       getAllPlayers(gameRoomId).then((players) => {
@@ -25,6 +39,9 @@ export const OnlineCard = () => {
     }
   }, [gameJoined]);
 
+  /**
+   * Creates a game room with the player's name.
+   */
   const createGame = () => {
     if (playerName.trim() == "") {
       setError("Please enter your name")
@@ -39,6 +56,10 @@ export const OnlineCard = () => {
     })
   }
 
+  /**
+   * Adds new players to the list of players.
+   * @param newPlayer
+   */
   const addPlayers = (newPlayer: IOnlinePlayer[]) => {
     setPlayers((players) => {
       return [...players, ...newPlayer];
@@ -46,6 +67,9 @@ export const OnlineCard = () => {
     console.log("Players", [...players, ...newPlayer]);
   }
 
+  /**
+   * Joins a game room with the given game ID and player name.
+   */
   const joinGame = () => {
     if (gameIdInput.trim() == "" || playerName.trim() == "") {
       setError("Please enter your name and game ID")
@@ -60,6 +84,9 @@ export const OnlineCard = () => {
     })
   }
 
+  /**
+   * Toggles the ready status of the player.
+   */
   const toggleReadyStatus = () => {
     setIsReady(!isReady);
   };
@@ -67,29 +94,34 @@ export const OnlineCard = () => {
   return (
     <div className={"flex flex-col h-full w-full"}>
       {!gameJoined && <div className={"flex flex-col h-full w-full items-center justify-center gap-2"}>
-        <input
+        {/* <input
           className={`border-b-[1px] text-xl ${playerName.trim() == "" ? "border-app-red" : "border-app-gray"} outline-0 text-center w-40 bg-transparent`}
           value={playerName}
           onChange={(e) => setPlayerName(e.target.value)}
           placeholder={"Your Name"}
-        />
-        <button
-          className="bg-app-yellow text-app-gray px-2 py-1 rounded-xl mx-1 w-48 border transition hover:scale-105 shadow"
-          onClick={createGame}>
-          CREATE A GAME ROOM
-        </button>
-        <div className="text-xl">OR</div>
+        /> */}
+        <div className={`text-xl font-bold ${playerName.trim() === "" ? "text-red-500" : "text-gray-800"} text-center w-40 mb-4`}>
+          {playerName || "Your Name"}
+        </div>
         <input
           className={`border-b-[1px] text-xl ${gameIdInput.trim() == "" ? "border-app-red" : "border-app-gray"} outline-0 text-center w-40 bg-transparent`}
           value={gameIdInput}
           onChange={(e) => setGameIdInput(e.target.value)}
           placeholder={"Enter Game ID"}
         />
-        <button
-          className="bg-app-yellow text-app-gray px-2 py-1 rounded-xl mx-1 w-48 border transition hover:scale-105 shadow"
-          onClick={joinGame}>
-          JOIN A GAME ROOM
-        </button>
+        <div className="flex justify-center items-center mt-10"> 
+          <button
+            className="bg-app-yellow text-app-gray px-2 py-1 rounded-xl mx-1 w-40 border transition hover:scale-105 shadow"
+            onClick={createGame}>
+            CREATE A GAME
+          </button>
+          <button
+            className={`bg-app-yellow text-app-gray px-2 py-1 rounded-xl mx-1 w-40 border transition hover:scale-105 shadow ${gameIdInput.trim() === "" ? 'bg-gray-300 text-gray-500' : ''}`}
+            onClick={joinGame}
+            disabled={gameIdInput.trim() === ""}>
+            JOIN A GAME
+          </button>
+        </div>
         <div className={"text-xs text-app-red"}>
           {error}
         </div>
