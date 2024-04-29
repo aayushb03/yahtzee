aa55f791df7dbaaae64b95bf32f70cfa34f115e1
Jess Xu <jxxu2@wisc.edu>
2024-04-28 22:55:38 -0500

    fix dice animation, add delay to account for rolling

--- __tests__/frontend/yahtzeeGame.test.tsx
+++ __tests__/frontend/yahtzeeGame.test.tsx
@@ -139,8 +139,8 @@ describe('DiceRow component', () => {
     await waitFor(async () => {
       setTimeout(() => {
         // Check if the first dice now has the diceSelected class
-        expect(dice[0]).toHaveClass('DiceAnimations_diceSelected__X1YEg');
-      }, 100); // Adjust the delay time as needed
+        expect(dice[0]).toHaveClass('diceSelected');
+      }, 2500); // Adjust the delay time as needed
     });  
 
   });
